b037d30055fab1e4edf403d6c23d713b5a6d9e2f
shakhobiddin <muxawa@gmail.com>
2023-08-28 11:53:02 +0500

    fix folder

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => '127.0.0.1',
+                'host' => 'localhost',
                 'port' => 6001,
-                'scheme' => 'https',
-                'useTLS' => true,
+                'scheme' => 'http',
+                'useTLS' => false,
                 'debug' => true,
                 'encrypted' => true,
                 'curl_options' => [
