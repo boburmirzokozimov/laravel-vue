93fe496c0aa7fb92f50f736b4839ecf972b5c429
shakhobiddin <muxawa@gmail.com>
2023-08-28 12:45:56 +0500

    fix folder

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,11 +5,10 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    key: '1',
+    cluster: 'mt1',
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 443,
-    enabledTransports: ['ws', 'wss'],
+    wssPort: 443
 });
 
