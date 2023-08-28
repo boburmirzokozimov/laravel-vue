86fa013a9e21882003182cd47be1dd2d7095f339
shakhobiddin <muxawa@gmail.com>
2023-08-28 11:55:32 +0500

    fix folder

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,16 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => 'localhost',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
-                'useTLS' => false,
-                'debug' => true,
-                'encrypted' => true,
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+//                'encrypted' => true,
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
