5d3fc345e03e3b368d462067314d0fbbb32e0d9f
Boburmirzo Kozimov <boburmirzo@sabgames.org>
2023-08-30 13:08:01 +0500

    Test

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Balance\BalanceReplenishController;
 use App\Http\Controllers\Api\Branch\BranchController;
-use App\Http\Controllers\Api\Chat\ChatController;
 use App\Http\Controllers\Api\Chat\ChatRoomController;
 use App\Http\Controllers\Api\Client\Crypto\CryptoController;
 use App\Http\Controllers\Api\Client\Metal\MetalController;
@@ -20,27 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['api', 'auth.token'])->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
 
-    Route::post('/chat-room', [ChatRoomController::class, 'store']);
-    Route::get('/chat-room/{chatRoom}', [ChatRoomController::class, 'history']);
-    Route::post('/chat', [ChatController::class, 'sendByClient']);
+    Route::controller(ChatRoomController::class)->group(function () {
+        Route::post('/chat-room', 'store');
+        Route::get('/chat-room/{chatRoom}', 'history');
+        Route::post('/chat', 'sendByClient');
+    });
 
-    Route::post('/help-request', [HelpRequestController::class, 'store']);
+    Route::controller(BalanceReplenishController::class)->group(function () {
+        Route::post('/balance', 'manage');
+        Route::post('/balance-requisite', 'requisite');
+        Route::get('/balance-transactions-history', 'history');
+    });
 
-    Route::post('/balance', [BalanceReplenishController::class, 'manage']);
-    Route::post('/balance-requisite', [BalanceReplenishController::class, 'requisite']);
-    Route::get('/balance-transactions-history', [BalanceReplenishController::class, 'history']);
-
-    Route::get('/keys', [KeyController::class, 'index']);
-    Route::get('/countries', CountryController::class);
-    Route::post('/cards', [VisaCardController::class, 'store']);
-    Route::get('/cards', [VisaCardController::class, 'cards']);
-    Route::post('/cards/{card}/manage', [VisaCardController::class, 'manage']);
-
-    Route::get('/branches', BranchController::class);
-
-    Route::post('/transaction', TransactionController::class);
-
-    Route::post('/uploadFile', UploadFileController::class);
+    Route::controller(VisaCardController::class)->group(function () {
+        Route::post('/cards', 'store');
+        Route::get('/cards', 'cards');
+        Route::post('/cards/{card}/manage', 'manage');
+    });
 
     Route::controller(MetalController::class)->group(function () {
         Route::get('/metal', 'index');
@@ -55,6 +50,22 @@ Route::middleware(['api', 'auth.token'])->group(function () {
         Route::post('/crypto/buy', 'buy');
         Route::post('/crypto/sell', 'sell');
     });
+
+    Route::controller(HelpRequestController::class)->group(function () {
+        Route::post('/help-request', 'store');
+    });
+
+    Route::controller(KeyController::class)->group(function () {
+        Route::post('/keys', 'index');
+    });
+
+    Route::get('/branches', BranchController::class);
+    Route::get('/countries', CountryController::class);
+    Route::post('/transaction', TransactionController::class);
+    Route::post('/uploadFile', UploadFileController::class);
 });
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/refresh', [AuthController::class, 'refresh']);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/refresh', 'refresh');
+});
