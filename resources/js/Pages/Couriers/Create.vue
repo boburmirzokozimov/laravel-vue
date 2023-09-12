a58b5a79a83ee7a0b2beefe00dcdb213cedca169
Boburmirzo Kozimov <boburmirzo@sabgames.org>
2023-09-12 15:14:39 +0500

    Test

--- resources/js/Pages/Couriers/Create.vue
+++ resources/js/Pages/Couriers/Create.vue
@@ -9,161 +9,164 @@ const toaster = createToaster({ /* options */});
 
 const emit = defineEmits(['close'])
 const props = defineProps({
-  countries: Object
+    countries: Object
 })
 let form = useForm({
-  fullName: '',
-  phone: '',
-  country: '',
-  status: '',
-  tel_type: 'RU'
+    fullName: '',
+    phone: '',
+    country: '',
+    status: '',
+    tel_type: 'RU'
 
 })
 
 const generateMask = computed(() => {
-  return telMasks[form.tel_type]
+    return telMasks[form.tel_type]
 })
 const handleEdit = () => {
-  form
-      .transform((data) => ({
-        ...data,
-        phone: form.phone.replaceAll(' ', '')
-            .replaceAll('-', '')
-            .replace('+', '')
-            .replace('(', '')
-            .replace(')', '')
-      })).post('/couriers', {
-    onSuccess: () => {
-      emit('close')
-      toaster.success('Successfully created')
-    }
-  })
+    form
+        .transform((data) => ({
+            ...data,
+            phone: form.phone.replaceAll(' ', '')
+                .replaceAll('-', '')
+                .replace('+', '')
+                .replace('(', '')
+                .replace(')', '')
+        })).post('/couriers', {
+        onSuccess: () => {
+            emit('close')
+            toaster.success('Successfully created')
+        }
+    })
 
 }
 </script>
 
 <template>
-  <Head>
-    <title>Edit Courier</title>
-  </Head>
+    <Head>
+        <title>Edit Courier</title>
+    </Head>
 
-  <div class="w-[500px] modal z-10">
-    <h1 class="mb-6">Create The Courier</h1>
+    <div class="w-[500px] modal z-10">
+        <h1 class="mb-6">Create The Courier</h1>
 
-    <form class="max-w-2xl mx-auto" method="post" @submit.prevent="handleEdit">
-      <div class="mb-6">
-        <label
-            class="block mb-2 uppercase font-bold text-sm text-gray-700"
-            for="fullName"
-        >
-          ФИО
-        </label>
+        <form class="max-w-2xl mx-auto" method="post" @submit.prevent="handleEdit">
+            <div class="mb-6">
+                <label
+                    class="block mb-2 uppercase font-bold text-sm text-gray-700"
+                    for="fullName"
+                >
+                    ФИО
+                </label>
 
-        <input
-            id="fullName"
-            v-model="form.fullName"
-            class="border border-gray-200 p-2 w-full rounded-2xl"
-            name="fullName"
-            type="text"
-        />
-        <div v-if="form.errors.fullName" class="text-red-500 text-sm">{{ form.errors.fullName }}</div>
-      </div>
+                <input
+                    id="fullName"
+                    v-model="form.fullName"
+                    class="border border-gray-200 p-2 w-full rounded-2xl"
+                    name="fullName"
+                    type="text"
+                />
+                <div v-if="form.errors.fullName" class="text-red-500 text-sm">{{ form.errors.fullName }}</div>
+            </div>
 
-      <div class="mb-6">
-        <label
-            class="block mb-2 uppercase font-bold text-sm text-gray-700"
-            for="phone"
-        >
-          Phone
-        </label>
-        <div class="flex">
-          <select
-              id=""
-              v-model="form.tel_type"
-              class="border border-gray-200 p-2 w-2/12 rounded-bl-2xl rounded-tl-2xl " name="">
-            <option value="RU">RU</option>
-            <option value="US">US</option>
-          </select>
-          <input
-              id="phone"
-              v-model="form.phone"
-              v-maska
-              :data-maska="generateMask"
-              :placeholder="generateMask"
-              class="border border-gray-200 p-2 w-full rounded-br-2xl rounded-tr-2xl"
-              name="phone"
-              type="text"
-          />
-        </div>
+            <div class="mb-6">
+                <label
+                    class="block mb-2 uppercase font-bold text-sm text-gray-700"
+                    for="phone"
+                >
+                    Phone
+                </label>
+                <div class="flex">
+                    <select
+                        id=""
+                        v-model="form.tel_type"
+                        class="border border-gray-200 p-2 w-2/12 rounded-bl-2xl rounded-tl-2xl " name="">
+                        <option value="RU">RU</option>
+                        <option value="US">US</option>
+                        <option value="UAE">US</option>
+                        <option value="KG">KG</option>
+                        <option value="UZB">UZB</option>
+                    </select>
+                    <input
+                        id="phone"
+                        v-model="form.phone"
+                        v-maska
+                        :data-maska="generateMask"
+                        :placeholder="generateMask"
+                        class="border border-gray-200 p-2 w-full rounded-br-2xl rounded-tr-2xl"
+                        name="phone"
+                        type="text"
+                    />
+                </div>
 
-        <div v-if="form.errors.phone" class="text-red-500 text-sm">{{ form.errors.phone }}</div>
-      </div>
+                <div v-if="form.errors.phone" class="text-red-500 text-sm">{{ form.errors.phone }}</div>
+            </div>
 
 
-      <div class="mb-6">
-        <label
-            class="block mb-2 uppercase font-bold text-sm text-gray-700"
-            for="country"
-        >
-          Страна
-        </label>
-        <select id="country" v-model="form.country" class="border border-gray-200 p-2 w-full rounded-2xl"
-                name="country"
-                required>
-          <option
-              v-for="country in props.countries"
-              :selected="form.country === country.name"
-              :value="country.name"
-              v-text="country.name"
-          ></option>
-        </select>
-        <div v-if="form.errors.country" class="text-red-500 text-sm">{{ form.errors.country }}</div>
-      </div>
+            <div class="mb-6">
+                <label
+                    class="block mb-2 uppercase font-bold text-sm text-gray-700"
+                    for="country"
+                >
+                    Страна
+                </label>
+                <select id="country" v-model="form.country" class="border border-gray-200 p-2 w-full rounded-2xl"
+                        name="country"
+                        required>
+                    <option
+                        v-for="country in props.countries"
+                        :selected="form.country === country.name"
+                        :value="country.name"
+                        v-text="country.name"
+                    ></option>
+                </select>
+                <div v-if="form.errors.country" class="text-red-500 text-sm">{{ form.errors.country }}</div>
+            </div>
 
-      <div class="mb-6">
-        <label
-            class="block mb-2 uppercase font-bold text-sm text-gray-700"
-            for="status"
-        >
-          Статус
-        </label>
-        <select id="status" v-model="form.status" class="border border-gray-200 p-2 w-full rounded-2xl"
-                name="status"
-                required>
-          <option
-              selected
-              value="active"
-          >
-            Активный
-          </option>
-          <option
-              value="inactive"
-          >
-            Неактивный
-          </option>
-          <option
-              value="blocked"
-          >
-            Блокированный
-          </option>
-        </select>
-        <div v-if="form.errors.country" class="text-red-500 text-sm">{{ form.errors.country }}</div>
-      </div>
+            <div class="mb-6">
+                <label
+                    class="block mb-2 uppercase font-bold text-sm text-gray-700"
+                    for="status"
+                >
+                    Статус
+                </label>
+                <select id="status" v-model="form.status" class="border border-gray-200 p-2 w-full rounded-2xl"
+                        name="status"
+                        required>
+                    <option
+                        selected
+                        value="active"
+                    >
+                        Активный
+                    </option>
+                    <option
+                        value="inactive"
+                    >
+                        Неактивный
+                    </option>
+                    <option
+                        value="blocked"
+                    >
+                        Блокированный
+                    </option>
+                </select>
+                <div v-if="form.errors.country" class="text-red-500 text-sm">{{ form.errors.country }}</div>
+            </div>
 
-      <div class="flex justify-end w-full">
-        <button :disabled="form.processing" class="bg-blue-500 p-2 rounded-2xl text-white" type="submit">Submit
-        </button>
-      </div>
-    </form>
-  </div>
+            <div class="flex justify-end w-full">
+                <button :disabled="form.processing" class="bg-blue-500 p-2 rounded-2xl text-white" type="submit">Submit
+                </button>
+            </div>
+        </form>
+    </div>
 
 </template>
 
 <style lang="scss" scoped>
 .modal {
-  @apply absolute top-0  bg-white px-4 py-6 rounded-xl z-20;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+    @apply absolute top-0  bg-white px-4 py-6 rounded-xl z-20;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -9,146 +9,149 @@ import {telMasks} from "@/telMask.js";
 const toaster = createToaster({ /* options */});
 
 const props = defineProps({
-  user: Object,
-  countries: Object,
-  roles: Object,
+    user: Object,
+    countries: Object,
+    roles: Object,
 })
 let form = useForm({
-  full_name: '',
-  phone: '',
-  country: '',
-  role: '',
-  tel_type: 'RU'
+    full_name: '',
+    phone: '',
+    country: '',
+    role: '',
+    tel_type: 'RU'
 })
 
 const generateMask = computed(() => {
-  return telMasks[form.tel_type]
+    return telMasks[form.tel_type]
 })
 
 const handleSubmit = () => {
-  form
-      .transform((data) => ({
-        ...data,
-        phone: form.phone.replaceAll(' ', '')
-            .replaceAll('-', '')
-            .replace('+', '')
-            .replace('(', '')
-            .replace(')', '')
-      }))
-      .post('/users', {
-        onSuccess: () => toaster.success('Successfully created'),
-      })
+    form
+        .transform((data) => ({
+            ...data,
+            phone: form.phone.replaceAll(' ', '')
+                .replaceAll('-', '')
+                .replace('+', '')
+                .replace('(', '')
+                .replace(')', '')
+        }))
+        .post('/users', {
+            onSuccess: () => toaster.success('Successfully created'),
+        })
 }
 
 </script>
 
 <template>
-  <Head>
-    <title>Create User</title>
-  </Head>
+    <Head>
+        <title>Create User</title>
+    </Head>
 
-  <div class="">
-    <div class="flex max-w-2xl mx-auto items-center justify-between">
-      <h1 class="mb-6">Create The User</h1>
-      <div>
-        <NavLink class="btn-success"
-                 href="/users"
-        >
-          <i class="fa fa-backward text-white"></i>
-        </NavLink>
-      </div>
-    </div>
-
-    <form class="max-w-2xl mx-auto" method="post" @submit.prevent="handleSubmit">
-      <div class="mb-6">
-        <label
-            class="block mb-2 uppercase font-bold text-sm text-gray-700"
-            for="name"
-        >
-          Full Name
-        </label>
-
-        <input
-            id="name"
-            v-model="form.full_name"
-            class="border border-gray-200 p-2 w-full rounded-2xl"
-            name="name"
-            type="text"
-        />
-        <div v-if="form.errors.full_name" class="text-red-500 text-sm">{{ form.errors.full_name }}</div>
-      </div>
-      <!--      <div class="mb-6">-->
-      <!--        <vue-tel-input-->
-      <!--            v-model="form.phone"-->
-      <!--            :onlyCountries="['UZ','RU','US']"-->
-      <!--            autocomplete-->
-      <!--            dropdownOptions.showDialCodeInSelection-->
-      <!--            dropdownOptions.showFlags-->
-      <!--            inputClasses="border border-gray-200 p-2 w-full rounded-2xl"-->
-      <!--            mode="international"-->
-      <!--            wrapperClasses="border border-gray-200 p-2 w-full rounded-2xl"/>-->
-      <!--      </div>-->
-      <div class="mb-6">
-        <label
-            class="block mb-2 uppercase font-bold text-sm text-gray-700"
-            for="phone"
-        >
-          Phone
-        </label>
-        <div class="flex">
-          <select
-              id=""
-              v-model="form.tel_type"
-              class="border border-gray-200 p-2 w-2/12 rounded-bl-2xl rounded-tl-2xl " name="">
-            <option value="RU">RU</option>
-            <option value="US">US</option>
-          </select>
-          <input
-              id="phone"
-              v-model="form.phone"
-              v-maska
-              :data-maska="generateMask"
-              :placeholder="generateMask"
-              class="border border-gray-200 p-2 w-full rounded-br-2xl rounded-tr-2xl"
-              name="phone"
-              type="text"
-          />
+    <div class="">
+        <div class="flex max-w-2xl mx-auto items-center justify-between">
+            <h1 class="mb-6">Create The User</h1>
+            <div>
+                <NavLink class="btn-success"
+                         href="/users"
+                >
+                    <i class="fa fa-backward text-white"></i>
+                </NavLink>
+            </div>
         </div>
 
-        <div v-if="form.errors.phone" class="text-red-500 text-sm">{{ form.errors.phone }}</div>
-      </div>
+        <form class="max-w-2xl mx-auto" method="post" @submit.prevent="handleSubmit">
+            <div class="mb-6">
+                <label
+                    class="block mb-2 uppercase font-bold text-sm text-gray-700"
+                    for="name"
+                >
+                    Full Name
+                </label>
 
-      <div class="mb-6">
-        <select id="category" v-model="form.country" class="border border-gray-200 p-2 w-full rounded-2xl"
-                name="category"
-                required>
-          <option v-for="country in props.countries" :value="country.name" v-text="country.name"></option>
-        </select>
-        <div v-if="form.errors.country" class="text-red-500 text-sm">{{ form.errors.country }}</div>
-      </div>
+                <input
+                    id="name"
+                    v-model="form.full_name"
+                    class="border border-gray-200 p-2 w-full rounded-2xl"
+                    name="name"
+                    type="text"
+                />
+                <div v-if="form.errors.full_name" class="text-red-500 text-sm">{{ form.errors.full_name }}</div>
+            </div>
+            <!--      <div class="mb-6">-->
+            <!--        <vue-tel-input-->
+            <!--            v-model="form.phone"-->
+            <!--            :onlyCountries="['UZ','RU','US']"-->
+            <!--            autocomplete-->
+            <!--            dropdownOptions.showDialCodeInSelection-->
+            <!--            dropdownOptions.showFlags-->
+            <!--            inputClasses="border border-gray-200 p-2 w-full rounded-2xl"-->
+            <!--            mode="international"-->
+            <!--            wrapperClasses="border border-gray-200 p-2 w-full rounded-2xl"/>-->
+            <!--      </div>-->
+            <div class="mb-6">
+                <label
+                    class="block mb-2 uppercase font-bold text-sm text-gray-700"
+                    for="phone"
+                >
+                    Phone
+                </label>
+                <div class="flex">
+                    <select
+                        id=""
+                        v-model="form.tel_type"
+                        class="border border-gray-200 p-2 w-2/12 rounded-bl-2xl rounded-tl-2xl " name="">
+                        <option value="RU">RU</option>
+                        <option value="US">US</option>
+                        <option value="UAE">US</option>
+                        <option value="KG">KG</option>
+                        <option value="UZB">UZB</option>
+                    </select>
+                    <input
+                        id="phone"
+                        v-model="form.phone"
+                        v-maska
+                        :data-maska="generateMask"
+                        :placeholder="generateMask"
+                        class="border border-gray-200 p-2 w-full rounded-br-2xl rounded-tr-2xl"
+                        name="phone"
+                        type="text"
+                    />
+                </div>
 
-      <div class="mb-6">
-        <select id="role" v-model="form.role" class="border border-gray-200 p-2 w-full rounded-2xl"
-                name="role"
-                required>
-          <option v-for="role in props.roles" :value="role.id" v-text="role.name.toUpperCase()"></option>
-        </select>
-        <div v-if="form.errors.role" class="text-red-500 text-sm">{{ form.errors.role }}</div>
-      </div>
+                <div v-if="form.errors.phone" class="text-red-500 text-sm">{{ form.errors.phone }}</div>
+            </div>
+
+            <div class="mb-6">
+                <select id="category" v-model="form.country" class="border border-gray-200 p-2 w-full rounded-2xl"
+                        name="category"
+                        required>
+                    <option v-for="country in props.countries" :value="country.name" v-text="country.name"></option>
+                </select>
+                <div v-if="form.errors.country" class="text-red-500 text-sm">{{ form.errors.country }}</div>
+            </div>
+
+            <div class="mb-6">
+                <select id="role" v-model="form.role" class="border border-gray-200 p-2 w-full rounded-2xl"
+                        name="role"
+                        required>
+                    <option v-for="role in props.roles" :value="role.id" v-text="role.name.toUpperCase()"></option>
+                </select>
+                <div v-if="form.errors.role" class="text-red-500 text-sm">{{ form.errors.role }}</div>
+            </div>
 
 
-      <div class="flex justify-end w-full">
-        <button :disabled="form.processing" class="bg-blue-500 p-2 rounded-2xl text-white" type="submit">Submit
-        </button>
-      </div>
-    </form>
-  </div>
+            <div class="flex justify-end w-full">
+                <button :disabled="form.processing" class="bg-blue-500 p-2 rounded-2xl text-white" type="submit">Submit
+                </button>
+            </div>
+        </form>
+    </div>
 
 </template>
 
 <style lang="scss" scoped>
 .modal {
-  @apply absolute top-0 w-1/6 bg-white px-4 py-6 rounded-xl z-20;
-  transform: translate(-200%, 50%);
+    @apply absolute top-0 w-1/6 bg-white px-4 py-6 rounded-xl z-20;
+    transform: translate(-200%, 50%);
 }
 </style>
